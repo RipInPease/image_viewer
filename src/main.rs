a8ab970a286c5b99320f0fd8c62f3f0b3813d1bb
Snorre Vandbakk Fjelde <snorre.vandbakk.fjelde@oneco.no>
2026-04-22 13:25:07 +0200

    Started work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, World!");
+use std::env::args;
+use std::io::{Read};
+use std::fs::{self, File};
+
+mod bmp;
+
+fn main()  {
+    let mut args = args();
+    args.next();
+    let file_path = match args.next() {
+        Some(file) => file,
+        None       => {println!("Please enter an image file to open"); return }
+    };
+
+    if !fs::exists(&file_path).unwrap() {
+        println!("Could not find file");
+        return;
+    }
+
+    let i: usize = 
+        (&file_path)
+            .chars().rev().enumerate()
+            .take_while(|(_, c)| c != &'.')
+            .max_by(|a, b| a.0.cmp(&b.0))
+            .unwrap().0;
+    let file_type = &file_path[file_path.len()-1 - i..];
+    
+    match file_type {
+        "bmp" => (),
+        _     => {println!("Unknown file type"); return}
+    }
 }
